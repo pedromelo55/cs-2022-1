4d9a65aacb1f39a9fde69644d93acd4a40e49874
Pedro Melo <80073531+pedromelo55@users.noreply.github.com>
2022-06-25 16:51:50 -0300

    Create ex5.py

---
+++ tarefa006/ex5.py
@@ -0,0 +1,7 @@
+print("Qual o valor do salário mínimo: ")
+salMin = float(input())
+
+print("Qual o valor do seu salário: ")
+salario = float(input())
+
+print("Quantidade de salários mínimos: ", round((salario/salMin),2))
